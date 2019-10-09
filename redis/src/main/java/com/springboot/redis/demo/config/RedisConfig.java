bb1b9a1ee2dbcff1ca5aee80d1253295175413ca
klc <272953087@qq.com>
2019-10-09 20:04:35 +0800

    缓存Object对象的序列化和反序列化配置

--- redis/src/main/java/com/springboot/redis/demo/config/RedisConfig.java
+++ redis/src/main/java/com/springboot/redis/demo/config/RedisConfig.java
@@ -1,10 +1,19 @@
 package com.springboot.redis.demo.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.cache.jcache.config.JCacheConfigurerSupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.*;
 
 /**
  * @author kobs
@@ -31,5 +40,45 @@ public class RedisConfig extends JCacheConfigurerSupport {
             }
             return sb.toString();
         };
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory)
+    {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        GenericJackson2JsonRedisSerializer jackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
+        // 设置值（value）的序列化采用FastJsonRedisSerializer。
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        // 设置键（key）的序列化采用StringRedisSerializer。
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+    /**
+     * 自定义cacheManager缓存管理器
+     */
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory factory)
+    {
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+
+        //解决查询缓存转换异常的问题
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+
+        //配置序列化(解决乱码的问题)
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer))
+                .disableCachingNullValues();
+
+        return RedisCacheManager.builder(factory).cacheDefaults(config).build();
     }
 }
---
+++ redis/src/main/java/com/springboot/redis/demo/model/TestModel.java
@@ -0,0 +1,33 @@
+package com.springboot.redis.demo.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author kobs
+ * @date 2019/10/8
+ */
+@Data
+public class TestModel implements Serializable {
+    private static final long serialVersionUID = 8615939525685789362L;
+
+    private String code;
+    private String name;
+
+    /** jackson的反序列化需要无参构造函数 */
+    public TestModel() {
+
+    }
+
+    public TestModel(String code, String name) {
+        super();
+        this.code = code;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "{code:" + getCode() + ", name:" + getName() + "}";
+    }
+}
